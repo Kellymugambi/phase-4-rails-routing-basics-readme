0b59ff8b7dc61be944d32ecf2a1fa4e45e5fb6ec
Kellymugambi <kellymgambi@.com>
2023-03-14 07:06:38 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'cheeses', to: 'cheeses#index'
+  # HTTP verb => get
+  # Path: => 'cheeses'
+  # Controller Action: => 'cheeses#index'  
+  # in the CheesesController will be a method called index that gets called when a user goes to /cheeses
+
+
+
 end
+
+
